cdd2eb70efae977831f34ce63361cf4fa9d2a2da
Bruno Codeman <brunobemfica@gmail.com>
2016-12-20 00:48:30 -0200

    corrigida a besteira de apagar arquivos

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,54 +1,39 @@
 class NotesController < ApplicationController
-  before_action :set_note, only: [:show, :edit, :update, :destroy]
-  respond_to :json
+  before_action :set_note, only: [:show, :update, :destroy]
+  before_action :authenticate_user!, 
+
   # GET /notes
   # GET /notes.json
   def index
-    @notes = Note.all
-    render :json => @notes
+    #@notes = Note.all
+    @notes = current_user.notes
   end
 
   # GET /notes/1
   # GET /notes/1.json
   def show
-    @note = Note.find_by_id(params[:id])
-    render :json => @note
-  end
-
-  # GET /notes/new
-  def new
-    @note = Note.new
-    @note.user = current_user
-  end
-
-  # GET /notes/1/edit
-  def edit
-        @note = Note.find(params[:id])
+    render json: @note
   end
 
   # POST /notes
   # POST /notes.json
   def create
-    @note = Note.new(note_params)
-    print(@note)
-    respond_to do |format|
-      if @note.save
-        format.json { render :show, status: :created, location: @note }
-      else
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    @note = Note.new(:content => params[:content], :user => current_user)
+    print("json note "+ @note.to_json)
+    if @note.save
+      render :show, status: :created, location: @note
+    else
+      render json: @note.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /notes/1
   # PATCH/PUT /notes/1.json
   def update
-    respond_to do |format|
-      if @note.update_attributes(note_params)
-        format.json { render :show, status: :ok, location: @note }
-      else
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    if @note.user == current_user && @note.update_attribute(:content, params[:content])
+      render :show, status: :ok, location: @note
+    else
+      render json: @note.errors, status: :unprocessable_entity
     end
   end
 
@@ -56,9 +41,6 @@ class NotesController < ApplicationController
   # DELETE /notes/1.json
   def destroy
     @note.destroy
-    respond_to do |format|
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -69,6 +51,6 @@ class NotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
-      params.require(:note).permit(:id, :content, :created_at)
+      params.fetch(:note, {}).permit(:content)
     end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,4 @@
 class Note < ApplicationRecord
-	attr_accessor :content
 	belongs_to :user
 	validates :content, presence: true
 	validates_length_of :content, :maximum => 256, :allow_blank => false
